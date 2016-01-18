d8a0fe56d42757f30321bc855870bf62bbeb0b00
Rick <st87402@gmail.com>
2016-01-18 22:02:24 +0800

    習題2-2
    修改版本

--- Rick22.java
+++ Rick22.java
@@ -3,17 +3,17 @@ import java.util.Scanner;
 public class Rick22 {
 	public static void main(String[] args) {
 		
-		Scanner good = new Scanner(System.in);
-   		System.out.print("請輸入數學成績:");
-		int a = good.nextInt();
-		System.out.print("請輸入英文成績:");
-		int b = good.nextInt();
-		System.out.print("請輸入國文成績:");
-		int c = good.nextInt();
-		
-			double d;
-			d=a+b+c;
-			System.out.printf("平均分數 = %.2f",d/3);
+	Scanner good = new Scanner(System.in);
+   	System.out.print("請輸入數學成績:");
+	int math = good.nextInt();
+	System.out.print("請輸入英文成績:");
+	int english = good.nextInt();
+	System.out.print("請輸入國文成績:");
+	int chinese = good.nextInt();
+	
+	double all ;
+	all=math+english+chinese;
+	System.out.printf("平均分數 = %.2f",(all)/3);
 	   
 	}
 }
