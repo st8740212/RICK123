2d1b3fe0cad6c265c6466c691d4c12c7b8565765
Rick <st87402@gmail.com>
2016-01-18 12:17:13 +0800

    java 2-6-2
    2-6-2

--- myfirst.java
+++ myfirst.java
@@ -6,9 +6,10 @@ public static void main(String[] args) {
 	int[] a;
 	a = new int[] {1,2,3};
 	int [] n= a;
-	System.out.println("a[0]="+a[0]+"  a[1]="+a[1]+"  a[2]="+a[2]);
-	System.out.println("n[0]="+n[0]+"  n[1]="+n[1]+"  n[2]="+n[2]);
-
+	System.out.println("a[0]="+a[0]+"\ta[1]="+a[1]+"\ta[2]="+a[2]);
+	System.out.println("n[0]="+n[0]+"\tn[1]="+n[1]+"\tn[2]="+n[2]);
+	
+	System.out.println("---------------");
 	
 	
 	
