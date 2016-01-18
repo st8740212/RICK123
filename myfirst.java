0eabcfe08efe809a4dacb5256813a313bb7f8815
Rick <st87402@gmail.com>
2016-01-18 11:59:42 +0800

    java 2-6
    2-6

---
+++ myfirst.java
@@ -0,0 +1,17 @@
+package loveyou;
+
+public class myfirst{
+public static void main(String[] args) {
+	
+	int[] a;
+	a = new int[] {1,2,3};
+	int [] n= a;
+	System.out.println("a[0]="+a[0]+"  a[1]="+a[1]+"  a[2]="+a[2]);
+	System.out.println("n[0]="+n[0]+"  n[1]="+n[1]+"  n[2]="+n[2]);
+
+	
+	
+	
+
+}
+}
