36045e4e3f5f6e3b5ed96e3cf7838c5fb9c79a22
Rick <st87402@gmail.com>
2016-01-19 13:13:18 +0800

    習題3測試
    測試

---
+++ Rick3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Rick3 {
+	public static void main(String[] args) {
+	Scanner test = new Scanner(System.in);
+	
+   	int num1,num2,num3,max;
+   	System.out.print("請輸入第一個數字");
+   	num1=test.nextInt();
+   	System.out.print("請輸入第二個數字");
+   	num2=test.nextInt();
+   	System.out.print("請輸入第三個數字");
+   	num3=test.nextInt();
+   	if (num1>num2){
+   		if(num1>num3) 				
+   			max = num1;
+   		else
+   			max = num3;
+    	}///5612321312313656//
+   	
+   	
+	}
+}
