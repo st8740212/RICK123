ed153e483228d9cb05f4979b2e65b3d75e61c226
Rick <st87402@gmail.com>
2016-01-20 21:04:40 +0800

    習題3-1(第三題卡住了)

--- Rick3.java
+++ Rick3.java
@@ -1,14 +1,18 @@
 public class Rick3 {
 	public static void main(String[] args) {
-		System.out.println("(B)");
-		for(int i = 5; i >= 1 ; i--){
-			for(int k = 1; k <= i;k++){
+		System.out.println("(C)");
+		int i,j,k;
+		for(i = 1; i <= 5 ; i++){
+			for(j= 4; j >=i ; j--){
+			System.out.print("*");
+			}
+			for(k = 1;k <= i; k++){				
 				System.out.print(k);
+				
 				}
 			System.out.println();
 		}
-	
-}
+	}
 }
 		
 	
