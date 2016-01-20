d5ed4686bb95ee5b7b6285b54fa48df27607ca8d
Rick <st87402@gmail.com>
2016-01-20 16:25:44 +0800

    習題3  do whille 簡例

--- Rick3.java
+++ Rick3.java
@@ -2,22 +2,40 @@ import java.util.Scanner;
 
 public class Rick3 {
 	public static void main(String[] args) {
-	Scanner test = new Scanner(System.in);
-	
-   	int num1,num2,num3,max;
-   	System.out.print("請輸入第一個數字");
-   	num1=test.nextInt();
-   	System.out.print("請輸入第二個數字");
-   	num2=test.nextInt();
-   	System.out.print("請輸入第三個數字");
-   	num3=test.nextInt();
-   	if (num1>num2){
-   		if(num1>num3) 				
-   			max = num1;
-   		else
-   			max = num3;
-    	}
-   	
-   	
+		Scanner test = new Scanner(System.in);
+		
+		int num1=36,num2=12;
+		String se1;
+		do{
+		System.out.println("***********四則運算選單************* ");
+		System.out.println("A..+");
+		System.out.println("B..-");
+		System.out.println("C..*");
+		System.out.println("D../");
+		System.out.println("OUT");
+		System.out.println("*******************************");
+			do{
+				System.out.print("請選擇功能: ");
+				se1=test.next().toUpperCase();
+			}while ("ABCDE".indexOf(se1)== -1 );
+			
+			switch (se1){
+				case "A":
+					System.out.printf("%d+%d = %d%n",num1,num2,num1+num2);
+					break;
+				case "B":
+					System.out.printf("%d-%d = %d%n",num1,num2,num1-num2);
+					break;
+				case "C":
+					System.out.printf("%d*%d = %d%n",num1,num2,num1*num2);
+					break;
+				case "D":
+					System.out.printf("%d/%d = %d%n",num1,num2,num1/num2);
+					break;
+				case "E":
+					System.out.println("OUTTTTTTTTTTTTT");
+					break;
+					}
+		}while(!(se1.equals("E")));
 	}
 }
