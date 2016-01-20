818ee645f590c24a4f1fa6864acf2356d17378ab
Rick <st87402@gmail.com>
2016-01-20 19:50:38 +0800

    習題3-1(第二個解答)

--- Rick3.java
+++ Rick3.java
@@ -1,7 +1,7 @@
 public class Rick3 {
 	public static void main(String[] args) {
 		System.out.println("(B)");
-		for(int i = 1; i <= 5 ; i++){
+		for(int i = 5; i >= 1 ; i--){
 			for(int k = 1; k <= i;k++){
 				System.out.print(k);
 				}
