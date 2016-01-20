ea637a317b789553ad3da7b38fc97aedfea37bb0
Rick <st87402@gmail.com>
2016-01-20 19:45:45 +0800

    Revert "習題3-1"
    This reverts commit fe8021041074cb1084a64d7e042cb328242e2556.

--- Rick3.java
+++ Rick3.java
@@ -1,17 +1,41 @@
-
+import java.util.Scanner;
 
 public class Rick3 {
 	public static void main(String[] args) {
-				
-		System.out.println("(B)");
+		Scanner test = new Scanner(System.in);
 		
-		for(int i = 1; i <= 5 ; i++){
+		int num1=36,num2=12;
+		String se1;
+		do{
+		System.out.println("***********四則運算選單************* ");
+		System.out.println("A..+");
+		System.out.println("B..-");
+		System.out.println("C..*");
+		System.out.println("D../");
+		System.out.println("OUT");
+		System.out.println("*******************************");
+			do{
+				System.out.print("請選擇功能: ");
+				se1=test.next().toUpperCase();
+			}while ("ABCDE".indexOf(se1)== -1 );
 			
-			for(int k = 1; k <= i;k++){
-				System.out.print(k);
-				}
-			System.out.println();
-		}
-	
-}
+			switch (se1){
+				case "A":
+					System.out.printf("%d+%d = %d%n",num1,num2,num1+num2);
+					break;
+				case "B":
+					System.out.printf("%d-%d = %d%n",num1,num2,num1-num2);
+					break;
+				case "C":
+					System.out.printf("%d*%d = %d%n",num1,num2,num1*num2);
+					break;
+				case "D":
+					System.out.printf("%d/%d = %d%n",num1,num2,num1/num2);
+					break;
+				case "E":
+					System.out.println("OUTTTTTTTTTTTTT");
+					break;
+					}
+		}while(!(se1.equals("E")));
+	}
 }
