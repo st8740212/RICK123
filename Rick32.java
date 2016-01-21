2391e6e0ed282e496ffbf8393e7bfe088ee89e8c
Rick <st87402@gmail.com>
2016-01-21 14:23:36 +0800

    習題3-2 (秒做一題)判斷基數還是偶數

---
+++ Rick32.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Rick32 {
+	public static void main(String[] args) {
+		Scanner good = new Scanner(System.in);
+		System.out.print("叫块计耞琌膀计临琌案计: ");
+		int number = good.nextInt();
+		if(number%2 == 0)
+			System.out.print("计案计");
+	}
+}
