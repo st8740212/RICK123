a5a9a898374d7fc26974a9e8ff13eb7ceaa91560
Rick <st87402@gmail.com>
2016-01-21 13:54:48 +0800

    習題3-1 正三角形

--- Rick3.java
+++ Rick3.java
@@ -4,15 +4,14 @@ public class Rick3 {
 		int i,j,k;
 		for(i = 1; i <= 5 ; i++){
 			for(j= 4; j >=i ; j--){
-			System.out.print("*");
+			System.out.print(" ");
 			}
-			for(k = 1;k <= i; k++){				
-				System.out.print(k);
+			for(k=1; k<=(2*i-1);k++){				
+				System.out.print(i);
 				
 				}
 			System.out.println();
 		}
 	}
-}
-		
+}	
 	
