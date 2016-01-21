7ab0e96427bb1ad81c4b64f26c3c3164ba87a935
Rick <st87402@gmail.com>
2016-01-21 15:10:15 +0800

    習題3-2( 輸入帳號密碼)

--- Rick32.java
+++ Rick32.java
@@ -2,11 +2,36 @@ import java.util.Scanner;
 public class Rick32 {
 	public static void main(String[] args) {
 		Scanner good = new Scanner(System.in);
-		System.out.print("請輸入一個數判斷是奇數還是偶數: ");
-		int number = good.nextInt();
-		if(number %2 == 0)
-			System.out.print("此數為偶數");
-		else
-			System.out.print("此數為奇數");
+		String InUserID,InUserPW;
+		String UserID="JAVA",UserPW="1234";
+		int i=1;
+		
+		a:while(i<4){
+		
+			System.out.print("請輸入帳號: ");
+			InUserID = good.next();
+			
+			System.out.print("請輸入密碼: ");
+			InUserPW = good.next();
+		
+				if (InUserID.equals(UserID) && InUserPW.equals(UserPW)) 
+				{
+					System.out.println("登入成功!");	
+					break a;
+				}
+				else
+				{
+					if(i<3)
+					{
+						System.out.println("輸入錯誤"+i+"次");	
+						i++;
+					}
+					else 
+					{
+						System.out.println("登入失敗!");
+						break a;
+					}
+				}
+		}
 	}
 }
