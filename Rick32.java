b59570b264e0d5d6a5c7578f336c8ce94842adc5
Rick <st87402@gmail.com>
2016-01-21 14:26:49 +0800

    習題3-2 小修改

--- Rick32.java
+++ Rick32.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 public class Rick32 {
 	public static void main(String[] args) {
 		Scanner good = new Scanner(System.in);
-		System.out.print("叫块计耞琌膀计临琌案计: ");
+		System.out.print("叫块计耞琌计临琌案计: ");
 		int number = good.nextInt();
-		if(number%2 == 0)
+		if(number %2 == 0)
 			System.out.print("计案计");
 		else
 			System.out.print("计计");
