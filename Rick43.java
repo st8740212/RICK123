b954f1efaae25a559ccbdfa9767342ef4d77db89
Rick <st87402@gmail.com>
2016-01-22 20:54:50 +0800

    習題 奇數偶數

--- Rick43.java
+++ Rick43.java
@@ -1,22 +1,23 @@
-
 public class Rick43 {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+		{
 		int[] list = { 36 , 77 , 15 , 28 , 33 , 10 , 58 , 70 , 41};  
-		reorderOddEven(list); 							
-	}
+		reorderOddEven(list); 
+		}
 		static void reorderOddEven(int[] list) 
 		{
+			
 		int length = list.length;                    
 		System.out.println("原本的值: ");
 		for (int i = 0; i < length; i++) 
 		{			
-			System.out.print(list[i] + " ");
+		System.out.print(list[i] + " ");
 		}
 		System.out.print("\n");
-		for (int i=0;i<list.length-1;i++)
+		for (int i = 0; i < list.length-1; i++)
         {
-             for (int j=0;j<list.length-1;j++)  
+             for (int j=0;j<list.length-1; j++)  
              {
                   if (list[j+1]<list[j])
                   {
@@ -26,19 +27,21 @@ public class Rick43 {
                   }
              }
         }
+		System.out.println("奇數偶數值: ");
+		
 		for (int i = 0; i < length; i++) 
+		{
+			if((list[i] & 0x1) !=0)
 			{
-			if((list[i] & 0x1)!=0)
-			{
-				System.out.print(list[i]+" ");
+			System.out.print(list[i]+" ");
 			}
-			}
+		}
 		for (int a = 0; a < length; a++) 
 		{
-				if((list[a] & 0x1)==0)
-				{
-				System.out.print(list[a]+" " );
-				}
+			if((list[a] & 0x1) ==0)
+			{
+			System.out.print(list[a]+" " );
 			}
 		}
 	}
+}
