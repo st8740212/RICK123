d266ec1b156290b224f4714afc2dcb71483f64e7
Rick <st87402@gmail.com>
2016-01-22 20:32:06 +0800

    二哥的題目(奇數在前偶數在後排序)

--- Rick43.java
+++ Rick43.java
@@ -5,14 +5,14 @@ public class Rick43 {
 		int[] list = { 36 , 77 , 15 , 28 , 33 , 10 , 58 , 70 , 41};  
 		reorderOddEven(list); 							
 	}
-		static void reorderOddEven(int[] list) {
+		static void reorderOddEven(int[] list) 
+		{
 		int length = list.length;                    
-		
 		System.out.println("原本的值: ");
-		for (int i = 0; i < length; i++) {			
+		for (int i = 0; i < length; i++) 
+		{			
 			System.out.print(list[i] + " ");
 		}
-		
 		System.out.print("\n");
 		for (int i=0;i<list.length-1;i++)
         {
@@ -26,13 +26,19 @@ public class Rick43 {
                   }
              }
         }
-		
-		if(  int[] list !=0 ){         ///抓不到排序後的值
-			System.out.print(  +", "); 
+		for (int i = 0; i < length; i++) 
+			{
+			if((list[i] & 0x1)!=0)
+			{
+				System.out.print(list[i]+" ");
+			}
+			}
+		for (int a = 0; a < length; a++) 
+		{
+				if((list[a] & 0x1)==0)
+				{
+				System.out.print(list[a]+" " );
+				}
+			}
 		}
-		
-		
-		
 	}
-
-}
