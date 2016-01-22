281788ff709b59ea8dea63fdad0de8e46a0fff3a
Rick <st87402@gmail.com>
2016-01-22 20:17:46 +0800

    習題43(抓不到值阿)

--- Rick43.java
+++ Rick43.java
@@ -1,32 +1,38 @@
+
 public class Rick43 {
 	
 	public static void main(String[] args) {
-		int[] list = { 18, 22, 53, 44, 85, 17, 69, 11 };
-		reorderOddEven(list);
+		int[] list = { 36 , 77 , 15 , 28 , 33 , 10 , 58 , 70 , 41};  
+		reorderOddEven(list); 							
 	}
-
-	public static void reorderOddEven(int[] list) {
-		int length = list.length;
-		for (int i = 0; i < length; i++) {
+		static void reorderOddEven(int[] list) {
+		int length = list.length;                    
+		
+		System.out.println("原本的值: ");
+		for (int i = 0; i < length; i++) {			
 			System.out.print(list[i] + " ");
 		}
+		
 		System.out.print("\n");
-		int begin = 0;
-		int end = length - 1;
-		while (begin < end) {
-			while (begin < end && (list[begin] & 0x1) != 0)
-				begin++;
-			while (begin < end && (list[end] & 0x1) == 0)
-				end--;
-			if (begin < end) {
-				int temp = list[begin];
-				list[begin] = list[end];
-				list[end] = temp;
-			}
+		for (int i=0;i<list.length-1;i++)
+        {
+             for (int j=0;j<list.length-1;j++)  
+             {
+                  if (list[j+1]<list[j])
+                  {
+                      int temp = list[j+1];
+                      list[j+1]=list[j];
+                      list[j]= temp;
+                  }
+             }
+        }
+		
+		if(  int[] list !=0 ){         ///抓不到排序後的值
+			System.out.print(  +", "); 
 		}
-		for (int i = 0; i < length; i++) {
-			System.out.print(list[i] + " ");
-		}
+		
+		
+		
 	}
 
 }
