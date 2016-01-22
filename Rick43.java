db8b107e3a220c6e99db5e640e965178c2b8480d
Rick <st87402@gmail.com>
2016-01-22 21:21:19 +0800

    習題(改)

--- Rick43.java
+++ Rick43.java
@@ -5,36 +5,37 @@ public class Rick43 {
 			int[] list = { 36 , 77 , 15 , 28 , 33 , 10 , 58 , 70 , 41};  
 			reorderOddEven(list); 
 		}
+	
 		static void reorderOddEven(int[] list) 
 		{
-			int length = list.length;     
-			
-			System.out.println("原本的值: ");
+		int length = list.length;
+		System.out.println("原本的值: ");
 		for (int i = 0; i < length; i++) 
 		{			
 			System.out.print(list[i] + " ");
 		}
-			System.out.print("\n");
-			System.out.println("奇數偶數的值: ");
+			
+		System.out.print("\n");
+		System.out.println("奇數偶數的值: ");
+		
 		for (int i = 0; i < list.length-1; i++)
         {
              for (int j = 0; j < list.length-1; j++)  
              {
-                  if (list[j]<list[j+1])
+                  if (list[j] < list[j+1])
                   {
                       int temp = list[j+1];
-                      list[j+1]=list[j];
-                      list[j]= temp;
+                      list[j+1] = list[j];
+                      list[j] = temp;
                   }
              }
         }
-		
-		
+
 		for (int i = 0; i < length; i++) 
 		{
-			if((list[i] & 0x1) !=0)
+			if((list[i] & 0x1) != 0)
 			{
-			System.out.print(list[i]+" ");
+				System.out.print(list[i] + " ");
 			}
 		}
 		
@@ -42,19 +43,20 @@ public class Rick43 {
         {
              for (int j = 0; j < list.length-1; j++)  
              {
-                  if (list[j+1]<list[j])
+                  if (list[j+1] < list[j])
                   {
                       int temp = list[j+1];
-                      list[j+1]=list[j];
-                      list[j]= temp;
+                      list[j+1] = list[j];
+                      list[j] = temp;
                   }
              }
         }
+		
 		for (int a = 0; a < length; a++) 
 		{
-			if((list[a] & 0x1) ==0)
+			if((list[a] & 0x1) == 0)
 			{
-			System.out.print(list[a]+" " );
+				System.out.print(list[a] + " " );
 			}
 		}
 	}
