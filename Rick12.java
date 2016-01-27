575523c2a5d4379518a613a3a1153cc45758190f
Rick <st87402@gmail.com>
2016-01-27 16:26:16 +0800

    目前寫出的

--- Rick12.java
+++ Rick12.java
@@ -1,36 +1,38 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.*;  
+import java.util.*;
+
 public class Rick12
 {
     public static void main(String[] args)
     {
-       /* Scanner scn = new Scanner(System.in);
-        System.out.println("歡迎來到我的99程式");
-        System.out.print("請輸入你的名子: ");
-        String name = scn.next();
-        System.out.println("歡迎" + name);
-        System.out.println("我們有四個人在玩遊戲");*/
-        System.out.println(card(0));
+        /*
+         * Scanner scn = new Scanner(System.in);
+         * System.out.println("歡迎來到我的99程式"); System.out.print("請輸入你的名子: ");
+         * String name = scn.next(); System.out.println("歡迎" + name);
+         * System.out.println("我們有四個人在玩遊戲");
+         */
+        System.out.println(card(52));
     }
 
     public static int card(int card)
     {
         ArrayList list = new ArrayList(52);
         
-        
-        for (int i = 1; i <= 13; i++)
+        for (int i = 1; i <= 4; i++)
         {
-            for (int j = 1; j <= 4; j++)
+            for (int j = 1; j <= 13; j++)
             {
-                
-               
+               int a=i+""+j;             //這邊沒辦法連結兩個數值
+                System.out.println(i+""+j);
                 
             }
         }
-        System.out.println(list.size());
         
-        return card; 
+        return card;
+        
+
+        
     }
 
 }
--- Rick14.java
+++ Rick14.java
@@ -1,55 +1,82 @@
 import java.util.*;
 
- class Card {
+class Card
+{
     private String suit;
     private String symbol;
-    
-    public Card(String suit, String symbol) {
+
+    public Card(String suit, String symbol)
+    {
         this.suit = suit;
         this.symbol = symbol;
     }
-    
-    public String toString() { return suit + symbol; }
+
+    public String toString()
+    {
+        return suit + symbol;
+    }
 }
 
-public class Rick14 
+public class Rick14
 {
     private static List<Card> cards = new ArrayList<>(52);
-    static {
-        for(int i = 0; i < 52; i++) {
+
+    static
+    {
+        for (int i = 0; i < 52; i++)
+        {
             cards.add(new Card(suit(i + 1), symbol(i + 1)));
         }
     }
-    private static String suit(int number) {
-        switch((number - 1) / 13) { 
-            case 0 : return "桃";
-            case 1 : return "心";
-            case 2 : return "磚";
-            default: return "梅";
+
+    private static String suit(int number)
+    {
+        switch ((number - 1) / 13)
+        {
+        case 0:
+            return "桃";
+        case 1:
+            return "心";
+        case 2:
+            return "磚";
+        default:
+            return "梅";
         }
     }
-    private static String symbol(int number) {
+
+    private static String symbol(int number)
+    {
         int remain = number % 13;
-        switch(remain) { 
-            case 0 : return String.format("%c ", 'K');
-            case 1 : return String.format("%c ", 'A');
-            case 11: return String.format("%c ", 'J');
-            case 12: return String.format("%c ", 'Q');
-            default: return String.format("%-2d", remain);
-        } 
+        switch (remain)
+        {
+        case 0:
+            return String.format("%c ", 'K');
+        case 1:
+            return String.format("%c ", 'A');
+        case 11:
+            return String.format("%c ", 'J');
+        case 12:
+            return String.format("%c ", 'Q');
+        default:
+            return String.format("%-2d", remain);
+        }
     }
-    public static Card[] shuffle() {
-        for(int i = 0; i < cards.size(); i++) {
-            Collections.swap(cards, i, 
-                (int) (Math.random() * cards.size() - 1));
+
+    public static Card[] shuffle()
+    {
+        for (int i = 0; i < cards.size(); i++)
+        {
+            Collections.swap(cards, i, (int) (Math.random() * cards.size() - 1));
         }
         return cards.toArray(new Card[52]);
     }
-    public static void main(String args[]) { 
+
+    public static void main(String args[])
+    {
         Card[] cards = shuffle();
-        for(int i = 0; i < cards.length; i++) {
-            System.out.printf(
-                "%s%c", cards[i], (i + 1) % 13 == 0 ? '\n' : ' '); 
+        for (int i = 0; i < cards.length; i++)
+        {
+            System.out.printf("%s%c", cards[i], (i + 1) % 13 == 0 ? '\n' : ' ');
         }
-    } 
+    }
 }
