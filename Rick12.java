42a9a2ca154400f101499dd2b539843bea2d03c8
Rick <st87402@gmail.com>
2016-01-27 17:42:00 +0800

    存入陣列

--- Rick12.java
+++ Rick12.java
@@ -1,7 +1,6 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.*;
-
+import java.util.Arrays;
 public class Rick12
 {
     public static void main(String[] args)
@@ -12,27 +11,15 @@ public class Rick12
          * String name = scn.next(); System.out.println("歡迎" + name);
          * System.out.println("我們有四個人在玩遊戲");
          */
-        System.out.println(card(52));
-    }
-
-    public static int card(int card)
-    {
-        ArrayList list = new ArrayList(52);
         
         for (int i = 1; i <= 4; i++)
         {
             for (int j = 1; j <= 13; j++)
             {
-               int a=i+""+j;             //這邊沒辦法連結兩個數值
-                System.out.println(i+""+j);
-                
+                int[] a={j};
             }
         }
-        
-        return card;
-        
-
-        
     }
 
+  
 }
