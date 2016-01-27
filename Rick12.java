524a2439175e21c85033d242d90d0ea9f7c506e2
Rick <st87402@gmail.com>
2016-01-27 14:14:35 +0800

    找araray 資料

--- Rick12.java
+++ Rick12.java
@@ -1,25 +1,37 @@
-
+import java.util.ArrayList;
 import java.util.Scanner;
-
+import java.util.*;  
 public class Rick12
 {
     public static void main(String[] args)
     {
-        Scanner scn = new Scanner(System.in);
+       /* Scanner scn = new Scanner(System.in);
         System.out.println("歡迎來到我的99程式");
         System.out.print("請輸入你的名子: ");
         String name = scn.next();
         System.out.println("歡迎" + name);
-        System.out.println("我們有四個人在玩遊戲");
+        System.out.println("我們有四個人在玩遊戲");*/
+        ArrayList list = new ArrayList();
+        System.out.println(list.size());
+        System.out.println(list.isEmpty());
+        list.add("楊過");  
+        list.add(1,"小龍女");      
+        System.out.println(list.size());
+        System.out.println(list.get(1));  
     }
 
-    public static void people(int player)
+    public static void card(int card)
     {
-        int player1, player2, player3, player4;
+       
+
+        for (int i = 0; i <= 13; i++)
+        {
+            for (int a = 0; a <= 4; a++)
+            {
+                System.out.println(card);
+            }
+        }
 
     }
 
-    
-
-    
 }
