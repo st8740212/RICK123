e53f4d46ab21cdada09c1348de38b8a27a5dd0ec
Rick <st87402@gmail.com>
2016-01-27 19:34:30 +0800

    新增刪掉陣列 以及讀取陣列大小

--- Rick12.java
+++ Rick12.java
@@ -10,7 +10,9 @@ public class Rick12
          * String name = scn.next(); System.out.println("歡迎" + name);
          * System.out.println("我們有四個人在玩遊戲");
          */
+        
         System.out.println(card(0));
+        
     }
 
     public static int card(int temp)
@@ -24,8 +26,12 @@ public class Rick12
                 lists.add(j);
             }
         }
-        
+        System.out.println(lists.size());
+        lists.remove(temp);
+        System.out.println(lists.size());
         return lists.get(temp);
+        
+        
         /*
         for (int var : lists)
         {
@@ -35,7 +41,7 @@ public class Rick12
         System.out.println("\n" + lists.get(35));
         */
     }
-    public static void wash()
+    public static void wash(int wash)
     {
         
     }
