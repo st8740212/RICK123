27115d220e0c451c698b85255afb939b2bb86e8f
Rick <st87402@gmail.com>
2016-01-28 19:39:26 +0800

    目前進度

--- Rick12.java
+++ Rick12.java
@@ -21,58 +21,72 @@ public class Rick12
             player4.add(getcard(cardlist));
         }
         int total = 0;
-        
-        for (int i = 1; i <52; i++)
+
+        for (int i = 1; i < 52; i++)
         {
-            if(total < 100){
-            System.out.println("玩家1出: "+player1.get(0));
-            total += player1.get(0);
-            player1.remove(0);
-            player1.add(getcard(cardlist));
-            System.out.println("目前分數: "+total);
+            if (total < 100)
+            {
+                System.out.println("玩家1出: " + player1.get(0));
+                total += player1.get(0);
+                player1.remove(0);
+                player1.add(getcard(cardlist));
+                System.out.println("目前分數: " + total);
             }
-            else {
+
+            else
+            {
+                System.out.println("玩家4輸了!!!");
+                break;
+            }
+
+            if (total < 100)
+            {
+                System.out.println("玩家2出: " + player2.get(0));
+                total += player2.get(0);
+                player2.remove(0);
+                player2.add(getcard(cardlist));
+                System.out.println("目前分數: " + total);
+            }
+
+            else
+            {
                 System.out.println("玩家1輸了!!!");
                 break;
             }
-            if(total < 100){
-            System.out.println("玩家2出: "+player2.get(0));
-            total +=  player2.get(0);
-            player2.remove(0);
-            player2.add(getcard(cardlist));
-            System.out.println("目前分數: "+total);
+
+            if (total < 100)
+            {
+                System.out.println("玩家3出: " + player3.get(0));
+                total += player3.get(0);
+                player3.remove(0);
+                player3.add(getcard(cardlist));
+                System.out.println("目前分數: " + total);
             }
-            
-            else {
-            System.out.println("玩家2輸了!!!");
+
+            else
+            {
+                System.out.println("玩家2輸了!!!");
                 break;
-                
             }
-            if(total < 100){
-            System.out.println("玩家3出: "+player3.get(0));
-            total += player3.get(0) ;
-            player3.remove(0);
-            player3.add(getcard(cardlist));
-            System.out.println("目前分數: "+total);
+
+            if (total < 100)
+            {
+                System.out.println("玩家4出: " + player4.get(0));
+                total += player4.get(0);
+                player4.remove(0);
+                player4.add(getcard(cardlist));
+                System.out.println("目前分數: " + total);
             }
-            else {
-            System.out.println("玩家3輸了!!!");
-            break;
+
+            else
+            {
+                System.out.println("玩家3輸了!!!");
+                break;
             }
-            if(total < 100){
-            System.out.println("玩家4出: "+player4.get(0));
-            total += player4.get(0);
-            player4.remove(0);
-            player4.add(getcard(cardlist));
-            System.out.println("目前分數: "+total);
+            if (total < 100)
+            {
+                System.out.println("-----------下一輪----------");
             }
-            
-            else {
-            System.out.println("玩家4輸了!!!");
-            break;
-            }
-            System.out.println("-----------下一輪----------");
- 
         }
 
     }
