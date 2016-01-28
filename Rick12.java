5a6f38f9779c3c0d07d80d615341869690c38c84
Rick <st87402@gmail.com>
2016-01-28 16:21:52 +0800

    給牌

--- Rick12.java
+++ Rick12.java
@@ -8,12 +8,18 @@ public class Rick12
     {
         ArrayList<Integer> cardlist = new ArrayList<Integer>();
         card(cardlist);
-       
-       
-        
-       
-
-
+        ArrayList<Integer> player1 = new ArrayList<Integer>();
+        ArrayList<Integer> player2 = new ArrayList<Integer>();
+        ArrayList<Integer> player3 = new ArrayList<Integer>();
+        ArrayList<Integer> player4 = new ArrayList<Integer>();
+        for(int i =1; i<=5; i++)
+        {
+        player1.add(getcard(cardlist));
+        player2.add(getcard(cardlist));
+        player3.add(getcard(cardlist));
+        player4.add(getcard(cardlist));
+        }
+        System.out.println(player1.size()+" "+player2.size()+" "+player3.size()+" "+player4.size());
     }
 
     public static void card(ArrayList<Integer> templist)
