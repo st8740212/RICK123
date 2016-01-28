482ec7ea2cccfa5d5d6da64c87375260b2cb2408
Rick <st87402@gmail.com>
2016-01-28 15:08:44 +0800

    random

--- Rick12.java
+++ Rick12.java
@@ -7,22 +7,23 @@ public class Rick12
     public static void main(String[] args)
     {
         ArrayList<Integer> cardlist = new ArrayList<Integer>();
+        Random ran = new Random();
         card(cardlist);
-        randomcard(cardlist);
-        toarray(cardlist);
- 
-        /* for (int i = 1; i <= 52; i++)
+        // System.out.println(cardlist.size());
+        // System.out.println(ran.nextInt(cardlist.size()));
+        // System.out.println(getcard(a, cardlist));
+        //你可以把getcard這個method改成直接取random的值
+        //你只要把random的那行code拿進來  取代原本的temp就可以了
+        
+        for (int i = 1; i <= 52; i++)
         {
-            int b = randomcard(cardlist);
-            System.out.println(getcard(b, cardlist));
-        }*/
-        for (int i = 0; i < toarray(cardlist).length; i++)
-        {
-            System.out.print(toarray(cardlist)[i] + " ");
+            System.out.println(getcard(ran.nextInt(cardlist.size()), cardlist));
         }
+    
+        
     }
 
-    public static void card(ArrayList<Integer> templist) /// 定義52張牌
+    public static void card(ArrayList<Integer> templist)
     {
 
         for (int i = 1; i <= 4; i++)
@@ -39,26 +40,13 @@ public class Rick12
          */
     }
 
-    public static int getcard(int temp, ArrayList<Integer> templist) /// 從牌庫拿取牌
+    public static int getcard(int temp, ArrayList<Integer> templist)
     {
         int number = templist.get(temp);
         templist.remove(temp);
-        // System.out.println(templist.size()); 排組範圍大小 {0 ~(templist.size()-1)}
-
+        // System.out.println(templist.size()); 範圍大小 {0 ~(templist.size()-1)}
+         
         return number;
-    }
-
-    public static int randomcard(ArrayList<Integer> random) // 把牌組隨機排列
-    {
-        Random ran = new Random();
-        int a = ran.nextInt(random.size());
-        return a;
-    }
-
-    public static Integer[] toarray(ArrayList<Integer> toarray)  
-    {
-        Integer[] values = toarray.toArray(new Integer[0]);
-        return values;
     }
 
 }
