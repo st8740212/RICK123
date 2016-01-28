33724fa1a380a2e08593586b6c31ddbd218df24e
Rick <st87402@gmail.com>
2016-01-28 11:24:09 +0800

    建立新的method

--- Rick12.java
+++ Rick12.java
@@ -7,26 +7,18 @@ public class Rick12
     public static void main(String[] args)
     {
         ArrayList<Integer> cardlist = new ArrayList<Integer>();
-        
-        
-        Random ran = new Random();
+       
         card(cardlist);
         // System.out.println(cardlist.size());
         // System.out.println(ran.nextInt(cardlist.size()));
         // int a=ran.nextInt(cardlist.size());
         // System.out.println(getcard(a, cardlist));
-        int b ;
-        
-        for (int i = 1; i <= 52; i++)
-        {
-            b = ran.nextInt(cardlist.size());
-            System.out.println(getcard(b, cardlist));
-        }
+        randomcard(cardlist);
         
         
     }
 
-    public static void card(ArrayList<Integer> templist)
+    public static void card(ArrayList<Integer> templist) /// 定義52張牌
     {
 
         for (int i = 1; i <= 4; i++)
@@ -43,13 +35,23 @@ public class Rick12
          */
     }
 
-    public static int getcard(int temp, ArrayList<Integer> templist)
+    public static int getcard(int temp, ArrayList<Integer> templist) /// 從牌庫拿取牌
     {
         int number = templist.get(temp);
         templist.remove(temp);
-        // System.out.println(templist.size()); ///範圍大小 {0 ~
-        // (templist.size()-1)}
+        // System.out.println(templist.size()); 範圍大小 {0 ~(templist.size()-1)}
+
         return number;
     }
 
+    public static void randomcard(ArrayList<Integer> random)  //把牌組隨機排列
+    {
+        Random ran = new Random();
+        int b = ran.nextInt(random.size());
+        for (int i = 1; i <= 52; i++) // 隨機亂數
+        {
+            b = ran.nextInt(random.size());
+            System.out.println(getcard(b, random));
+        }
+    }
 }
