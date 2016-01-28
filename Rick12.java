fe9e498561b54abcd16f8cdf6b992649c50e79d9
Rick <st87402@gmail.com>
2016-01-28 16:42:24 +0800

    做出選牌

--- Rick12.java
+++ Rick12.java
@@ -1,17 +1,18 @@
 import java.util.*;
 import java.util.Random;
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class Rick12
 {
     public static void main(String[] args)
     {
         ArrayList<Integer> cardlist = new ArrayList<Integer>();
-        card(cardlist);
         ArrayList<Integer> player1 = new ArrayList<Integer>();
         ArrayList<Integer> player2 = new ArrayList<Integer>();
         ArrayList<Integer> player3 = new ArrayList<Integer>();
         ArrayList<Integer> player4 = new ArrayList<Integer>();
+        card(cardlist);
+        Scanner scn = new Scanner(System.in);
         for(int i =1; i<=5; i++)
         {
         player1.add(getcard(cardlist));
@@ -19,10 +20,11 @@ public class Rick12
         player3.add(getcard(cardlist));
         player4.add(getcard(cardlist));
         }
-        System.out.println(player1.get(1)+" "+player1.get(2)+" "+player1.get(3)+" "+player1.get(4)+" "+player1.get(0));
-        System.out.println(player2.get(1)+" "+player2.get(2)+" "+player2.get(3)+" "+player2.get(4)+" "+player2.get(0));
-        System.out.println(player3.get(1)+" "+player3.get(2)+" "+player3.get(3)+" "+player3.get(4)+" "+player3.get(0));
-        System.out.println(player4.get(1)+" "+player4.get(2)+" "+player4.get(3)+" "+player4.get(4)+" "+player4.get(0));
+        
+        System.out.println("["+player1.get(0)+"] ["+player1.get(1)+"] ["+player1.get(2)+"] ["+player1.get(3)+"] ["+player1.get(4)+"]");
+        System.out.print("輸入你的選擇: ");
+        int choose = scn.nextInt();
+        
     }
 
     public static void card(ArrayList<Integer> templist)
