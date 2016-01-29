ad172f0fc70de326d6814f8e691a8a4aa6171721
Rick <st87402@gmail.com>
2016-01-29 17:38:58 +0800

    增加K牌特殊牌

--- Rick12.java
+++ Rick12.java
@@ -13,7 +13,77 @@ public class Rick12
         ArrayList<Integer> player4 = new ArrayList<Integer>();
         card(cardList);
         Scanner scn = new Scanner(System.in);
+        playerCardList(cardList, player1, player2, player3, player4);
 
+//        System.out.println("歡迎來到我的99遊戲");
+//        System.out.print("請輸入你的姓名: ");
+//        int name = scn.nextInt();
+//        System.out.println("歡迎" + name);
+//        System.out.println("--------------開始遊戲--------------");
+        
+        int total = 0;
+
+        for (;;)
+        {
+//            total += allPlayer(player1, cardList, 1, playerOne(player1));
+//            System.out.println("目前分數: " + total);
+//            System.out.println("**********");
+//            if (total > 99)
+//            {
+//                System.out.println("玩家1輸了!!!");
+//                break;
+//            }
+
+            total=specialCard_K( allPlayer(player2, cardList, 2, computer(player2, total)),total);
+            
+            System.out.println("目前分數: " + total);
+            System.out.println("**********");
+            if (total > 99)
+            {
+                System.out.println("玩家2輸了!!!");
+                break;
+            }
+//            
+//            total += allPlayer(player3, cardList, 3, computer(player3, total));
+//            System.out.println("目前分數: " + total);
+//            System.out.println("**********");
+//            if (total > 99)
+//            {
+//                System.out.println("玩家3輸了!!!");
+//                break;
+//            }
+//
+//            total += allPlayer(player4, cardList, 4, computer(player4, total));
+//            System.out.println("目前分數: " + total);
+//
+//            if (total > 99)
+//            {
+//                System.out.println("玩家4輸了!!!");
+//                break;
+//            }
+
+            System.out.println("-----------下一輪----------");
+        }
+
+        
+    }
+    public static int specialCard_K(int kcard,int total)
+    {
+        if(kcard==13)
+        {
+            total=99;
+        }
+        else
+        {
+            total=total+kcard;
+        }
+        return total;
+    }
+    
+    
+    private static void playerCardList(ArrayList<Integer> cardList, ArrayList<Integer> player1,
+            ArrayList<Integer> player2, ArrayList<Integer> player3, ArrayList<Integer> player4)
+    {
         for (int i = 1; i <= 5; i++)
         {
             player1.add(getCard(cardList));
@@ -21,77 +91,24 @@ public class Rick12
             player3.add(getCard(cardList));
             player4.add(getCard(cardList));
         }
-        
-//        System.out.println("歡迎來到我的99遊戲");
-//        System.out.print("請輸入你的姓名: ");
-//        int name = scn.nextInt();
-//        System.out.println("歡迎" + name);
-//        System.out.println("--------------開始遊戲--------------");
-        int total = 0;
-        for (;;)
-        {
-            
-            total += allPlayer(player1, cardList, 1, playerOne(player1));
-            System.out.println("目前分數: " + total);/// player1 total cardlist
-            System.out.println("**********");
-            if (total > 100)
-            {
-                System.out.println("玩家1輸了!!!");
-                break;
-            }
-           
-            total += allPlayer(player2, cardList, 2, computer(player2,total));
-            System.out.println("目前分數: " + total);/// player2 total cardlist
-            System.out.println("**********");
-            if (total > 100)
-            {
-                System.out.println("玩家2輸了!!!");
-                break;
-            }
-////
-            total += allPlayer(player3, cardList, 3, computer(player3,total));
-            System.out.println("目前分數: " + total);/// player3 total cardlist
-            System.out.println("**********");
-            if (total > 100)
-            {
-                System.out.println("玩家3輸了!!!");
-                break;
-            }
-
-            total += allPlayer(player4, cardList, 4, computer(player4,total));
-            System.out.println("目前分數: " + total);// player4 total cardlist
-
-            if (total > 100)
-            {
-                System.out.println("玩家4輸了!!!");
-                break;
-            }
-
-            System.out.println("-----------下一輪----------");
-        }
-
     }
 
-    public static int computer(ArrayList<Integer> player,int total)
+    public static int computer(ArrayList<Integer> player, int total)
     {
         int choose;
-
-        if(total<=70)
+        if (total <= 70)
         {
             choose = 5;
-           
-        }
-        else
+        } else
         {
-            choose =1;
+            choose = 1;
         }
-        
         return choose;
     }
-    
+
     public static int playerOne(ArrayList<Integer> player)
     {
-        Scanner scn = new Scanner(System.in); // 玩家輸入
+        Scanner scn = new Scanner(System.in); 
         System.out.println("我的手牌");
         Collections.sort(player);
         for (int i = 1; i <= 5; i++)
@@ -115,7 +132,7 @@ public class Rick12
         return playergetcard;
     }
 
-    public static void card(ArrayList<Integer> templist) /// 定義52張牌
+    public static void card(ArrayList<Integer> templist) 
     {
         for (int i = 1; i <= 4; i++)
         {
@@ -126,7 +143,7 @@ public class Rick12
         }
     }
 
-    public static int getCard(ArrayList<Integer> templist) // 隨機亂數取牌
+    public static int getCard(ArrayList<Integer> templist) 
     {
         Random ran = new Random();
         int temp = ran.nextInt(templist.size());
